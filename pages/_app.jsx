554c2d71d78a3ca45c1867ba0749f16a2751431f
Mert Çelik <76150391+mertcelik2196@users.noreply.github.com>
2021-07-23 14:30:14 +0300

    Add files via upload

---
+++ pages/_app.jsx
@@ -0,0 +1,41 @@
+import Head from "next/head";
+
+export default function App({ Component, pageProps }) {
+    return (
+        <>
+            <Head>
+                <meta name="twitter:card" content="summary" />
+                <meta name="twitter:url" content="https://mert.is-a.dev" />
+                <meta name="twitter:title" content="Mêrt" />
+                <meta name="twitter:description" content="Hello, My Name Is Mert. I'm Full-Stack Developer" />
+                <meta name="twitter:image" content="https://mert.is-a.dev/logo.png" />
+                <meta name="twitter:creator" content="@Mêrt" />
+                <meta name="theme-color" content="#09C29E" />
+                <meta name="robots" content="follow, index" />
+                <meta name="author" content="Mêrt" />
+
+                <meta property="og:type" content="website" />
+                <meta property="og:title" content="Mêrt#2196" />
+                <meta property="og:description" content="Hello, My Name Is Mert. I'm Full-Stack Developer" />
+                <meta property="og:site_name" content="Mert Çelik" />
+                <meta property="og:url" content="https://mert.is-a.dev" />
+                <meta property="og:image" content="https://mert.is-a.dev/logo.png" />
+
+                <link rel="shortcut icon" href="https://mert.is-a.dev/logo.png" />
+                <title>Mert</title>
+
+                {/* CSS */}
+                <link rel="stylesheet" href="/styles/bootstrap.min.css" />
+                <link rel="stylesheet" href="/styles/stylesheet.css" />
+
+                {/* JS */}
+                <script src="/scripts/jquery.min.js"></script>
+                <script src="/scripts/bootstrap.min.js"></script>
+                <script src="https://kit.fontawesome.com/f1c8ddd589.js" crossorigin="anonymous"></script>
+                {/* <script src="/scripts/script.js" defer></script> */}
+            </Head>
+
+            <Component {...pageProps} />
+        </>
+    )
+}
---
+++ pages/index.jsx
@@ -0,0 +1,11 @@
+import Home from "../components/Home";
+
+export default function index() {
+    return (
+        <div className="__page__root__ background">
+            <div className="jumbotron bg-transparent text-white">
+                <Home />
+            </div>
+        </div>
+    )
+}
