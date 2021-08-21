6412bc80cbf350b3f7fd10cb84bc226a80a915bb
mertcelik2196 <ac206031@gmail.com>
2021-08-21 12:44:54 +0300

    main

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 
+
 export default function App({ Component, pageProps }) {
     return (
         <>
@@ -22,17 +23,18 @@ export default function App({ Component, pageProps }) {
                 <meta property="og:image" content="https://mert.vercel.app/logo.gif" />
 
                 <link rel="shortcut icon" href="https://mert.vercel.app/logo.gif" />
-                <title>Mert</title>
+                <title>
+                    Mert
+                </title>
 
                 {/* CSS */}
-                <link rel="stylesheet" href="/styles/bootstrap.min.css" />
-                <link rel="stylesheet" href="/styles/stylesheet.css" />
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous" />
+                <link rel="stylesheet" href="/css/index.css" />
 
                 {/* JS */}
-                <script src="/scripts/jquery.min.js"></script>
-                <script src="/scripts/bootstrap.min.js"></script>
-                <script src="https://kit.fontawesome.com/f1c8ddd589.js" crossorigin="anonymous"></script>
-                {/* <script src="/scripts/script.js" defer></script> */}
+                <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-eMNCOe7tC1doHpGoWe/6oMVemdAVTMs2xqW4mwXrXsW0L84Iytr2wi5v2QjrP/xp" crossorigin="anonymous"></script>
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" integrity="sha384-cn7l7gDp0eyniUwwAZgrzD06kc/tftFf19TOAs2zVinnD/C7E91j9yyk5//jjpt/" crossorigin="anonymous"></script>
+                <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
             </Head>
 
             <Component {...pageProps} />
